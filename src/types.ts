cffa03abd0f0d9e0b9126749db36d9ff6ee7c252
rodrigoross <rodrigo.ross.comp@gmail.com>
2021-12-18 21:39:17 -0400

    criado interface para Eventos

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+// Centraliza onde todos os tipos de objetos serão usados no projeto.
+
+export interface EventItem {
+  id: number
+  category: string
+  title: string
+  description: string
+  location: string
+  date: string
+  time: string
+  organizer: string
+}
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -10,6 +10,7 @@ import { defineComponent } from 'vue'
 
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService'
+import { EventItem } from '@/types'
 
 export default defineComponent({
   name: 'EventList',
@@ -18,7 +19,7 @@ export default defineComponent({
   },
   data() {
     return {
-      events: null
+      events: [] as EventItem[]
     }
   },
   created() {
